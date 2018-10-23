bd6f2e7db64b7ebbd1e869387b3430c92e76415c
luc4hub <luca.licheri93@gmail.com>
2018-10-23 15:29:08 +0200

    metodi abilita e disabilita

--- src/it/controllo/ProvaUtenteAdmin.java
+++ src/it/controllo/ProvaUtenteAdmin.java
@@ -1,16 +1,46 @@
 package it.controllo;
 
+import it.modello.Utente;
+import it.persistenza.implementazione.DAOException;
 import it.persistenza.implementazione.DAOUtenteAdmin;
 
 public class ProvaUtenteAdmin {
 
 	public static void main(String[] args) {
 		DAOUtenteAdmin daoUtenteAdmin = new DAOUtenteAdmin();
+
 		
 //		daoUtenteAdmin.contaUtenti();
 //		daoUtenteAdmin.contaGruppi();
+//		daoUtenteAdmin.contaGruppiCompletati();
+//		daoUtenteAdmin.contaGruppiNonCompletati();
+//		daoUtenteAdmin.gradimentoAttivita();
+//		abilitaUtente(22L);
+//		disabilitaUtente(22L);
+//		daoUtenteAdmin.contaGruppiAttivita(2L);
 		
-		
+	}
+	
+	public static void abilitaUtente (Long id) {
+		DAOUtenteAdmin daoUtenteAdmin = new DAOUtenteAdmin();
+		Utente utente = new Utente();
+		utente.setId(id);
+		try {
+			daoUtenteAdmin.abilita(id);
+		} catch (DAOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void disabilitaUtente (Long id) {
+		DAOUtenteAdmin daoUtenteAdmin = new DAOUtenteAdmin();
+		Utente utente = new Utente();
+		utente.setId(id);
+		try {
+			daoUtenteAdmin.disabilita(id);
+		} catch (DAOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	
--- src/it/persistenza/implementazione/DAOUtenteAdmin.java
+++ src/it/persistenza/implementazione/DAOUtenteAdmin.java
@@ -7,7 +7,42 @@ import java.sql.SQLException;
 
 public class DAOUtenteAdmin extends DAOUtente {
 	
-	public void contaUtenti () {
+	
+	
+	public void abilita (Long id) throws DAOException {
+		Connection connection = DataSource.getInstance().getConnection();
+		PreparedStatement statement = null;
+		try {
+			statement = connection.prepareStatement("UPDATE UTENTE SET ABILITATO = 1 WHERE ID = ?");
+			statement.setLong(1, id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e.getMessage(), e);
+		} finally {
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+	}
+	
+	public void disabilita (Long id) throws DAOException {
+		Connection connection = DataSource.getInstance().getConnection();
+		PreparedStatement statement = null;
+		try {
+			statement = connection.prepareStatement("UPDATE UTENTE SET ABILITATO = 0 WHERE ID = ?");
+			statement.setLong(1, id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException(e.getMessage(), e);
+		} finally {
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+	}
+	
+	
+	public int contaUtenti () {
 		Connection connection = null;
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
@@ -18,7 +53,6 @@ public class DAOUtenteAdmin extends DAOUtente {
 			resultSet = statement.executeQuery();
 			if (resultSet.next()) {
 				numeroUtenti = resultSet.getInt(1);
-				System.out.println(numeroUtenti);
 			}
 		} catch (SQLException | DAOException e) {
 			System.out.println(e.getMessage());
@@ -27,10 +61,11 @@ public class DAOUtenteAdmin extends DAOUtente {
 			DataSource.getInstance().close(statement);
 			DataSource.getInstance().close(connection);
 		}
+		return numeroUtenti;
 	}
 	
 	
-	public void contaGruppi () {
+	public int contaGruppi () {
 		Connection connection = null;
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
@@ -41,7 +76,6 @@ public class DAOUtenteAdmin extends DAOUtente {
 			resultSet = statement.executeQuery();
 			if (resultSet.next()) {
 				numeroGruppi = resultSet.getInt(1);
-				System.out.println(numeroGruppi);
 			}
 		} catch (SQLException | DAOException e) {
 			System.out.println(e.getMessage());
@@ -50,5 +84,75 @@ public class DAOUtenteAdmin extends DAOUtente {
 			DataSource.getInstance().close(statement);
 			DataSource.getInstance().close(connection);
 		}
+		return numeroGruppi;
+	}
+	
+	public int contaGruppiCompletati () {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		int numeroGruppiCompletati = 0;
+		try {
+			connection = DataSource.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT COUNT(ID) FROM GRUPPO WHERE COMPLETO = 1");
+			resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				numeroGruppiCompletati = resultSet.getInt(1);
+			}
+		} catch (SQLException | DAOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			DataSource.getInstance().close(resultSet);
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+		return numeroGruppiCompletati;
+	}
+	
+	public int contaGruppiNonCompletati () {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		int numeroGruppiNonCompletati = 0;
+		try {
+			connection = DataSource.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT COUNT(ID) FROM GRUPPO WHERE COMPLETO = 0");
+			resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				numeroGruppiNonCompletati = resultSet.getInt(1);
+			}
+		} catch (SQLException | DAOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			DataSource.getInstance().close(resultSet);
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+		return numeroGruppiNonCompletati;
+	}
+	
+	
+	public int contaGruppiAttivita(Long id) {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		int numeroGruppiAttivita = 0;
+		try {
+			connection = DataSource.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT COUNT (ID) FROM GRUPPO WHERE ID_ATTIVITA = ?");
+			statement.setLong(1, id);
+			resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				numeroGruppiAttivita = resultSet.getInt(1);
+				System.out.println(numeroGruppiAttivita);
+			}
+		} catch (SQLException | DAOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			DataSource.getInstance().close(resultSet);
+			DataSource.getInstance().close(statement);
+			DataSource.getInstance().close(connection);
+		}
+		return numeroGruppiAttivita;
 	}
 }
